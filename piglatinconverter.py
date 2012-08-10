ed5160a27c630231b985135d8a8a9baa349e4a37
Drew Boshardy <dbosh09@gmail.com>
2012-08-10 16:03:45 -0500

    changed

--- piglatinconverter.py
+++ piglatinconverter.py
@@ -4,17 +4,17 @@ word = original.lower()
 first = word[0]
 if len(original) != 0 and original.isalpha():
 	# Rewrite next line using "if BLAH in (A, B, C)" syntax
-	if first=='a' or first=='o' or first=='i' or first=='e' or first=='u':
+	if first==("a","e","i","o","u")
 		print "vowel"
                 # Do this without converting strings to strings:
-		new_word = str(word) + str(pyg)
+		new_word = word + pyg
 		print new_word
 	else: 
 		print "consonant"
 		first_letter = word[0:1]
 		word_1 = word[1:len(word)]
                 # Do this without converting strings to strings:
-		new_word = str(word_1) + str(first_letter) + str(pyg)
+		new_word = word_1 + first_letter + pyg
 		print new_word
 else:
 	print 'empty'
@@ -22,5 +22,6 @@ else:
 # Are you testing enough cases?  What if someone passes "123"?  That's
 # not empty, but it's not pig-latinable either.  Expand the error
 # condition handling to cover every conceivable case.
+# Shouldn't the original.isalpha() check for numbers?
 
 # Other than the above, code looks good, btw!
