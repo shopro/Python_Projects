d480241e47b79ffe9ef20a05017751c5a7265ede
Karl Fogel <kfogel@red-bean.com>
2012-08-10 14:42:10 -0500

    Add some comments.

--- piglatinconverter.py
+++ piglatinconverter.py
@@ -3,16 +3,24 @@ pyg = 'ay'
 word = original.lower()
 first = word[0]
 if len(original) != 0 and original.isalpha():
+	# Rewrite next line using "if BLAH in (A, B, C)" syntax
 	if first=='a' or first=='o' or first=='i' or first=='e' or first=='u':
 		print "vowel"
+                # Do this without converting strings to strings:
 		new_word = str(word) + str(pyg)
 		print new_word
 	else: 
 		print "consonant"
 		first_letter = word[0:1]
 		word_1 = word[1:len(word)]
+                # Do this without converting strings to strings:
 		new_word = str(word_1) + str(first_letter) + str(pyg)
 		print new_word
 else:
 	print 'empty'
 
+# Are you testing enough cases?  What if someone passes "123"?  That's
+# not empty, but it's not pig-latinable either.  Expand the error
+# condition handling to cover every conceivable case.
+
+# Other than the above, code looks good, btw!
--- tripcalculator.py
+++ tripcalculator.py
@@ -2,6 +2,8 @@ def hotelCost(days):
     return 140*days
 
 def planeRideCost(city):
+    # Instead of using a conditional chain for this, can you
+    # rewrite it using a Python dictionary data structure?
     if city == "Charlotte":
         return 183 
     if city == "Tampa":
@@ -19,7 +21,13 @@ def rentalCarCost(days):
         cost -= 20
     return cost 
    
+# Your indentation levels above differ from the level below.
+# While the code will still work, that's bad form.  Pick a level and
+# stick to it across all function internals, etc.  I suggest either 2
+# or 4 spaces, using actual spaces not TAB characters.
+
 def tripCost(city,days, spendingMoney):
+	# The change above will have to be adjusted for here of course.
 	return spendingMoney + rentalCarCost(days) + hotelCost(days) + planeRideCost(city)
 
 print tripCost("Pittsburgh",7,600)
